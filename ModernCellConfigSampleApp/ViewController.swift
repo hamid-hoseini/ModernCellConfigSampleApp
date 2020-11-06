b34fc7a3a4181338bf0e9cc4e6b6350a3aee8ad4
Hamid <mail.hamid.hoseini@gmail.com>
2020-11-05 19:51:50 -0800

    Initial Commit

---
+++ ModernCellConfigSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCellConfigSampleApp
+//
+//  Created by Hamid Hoseini on 11/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
